bfa834f846c503714f351772a39987882c38181c
Adam Smith <iihtimsad@gmail.com>
2014-09-01 13:16:06 -0400

    Added heap test with random numbers.

--- tests/heap.c
+++ tests/heap.c
@@ -40,7 +40,31 @@ TEST Heap_basic_test()
 }
 
 
+// Fills heap with random numbers and checks that they're popped
+// off in order relative to eachother.
+TEST Heap_random_test()
+{
+    SETUP_HEAP();
+    for(int i = 0; i < 100; ++i){
+        int n = rand();
+        Heap_insert(&heap, &n);
+    }
+    int prev;
+    Heap_fetchTop(&heap, &prev);
+    Heap_removeTop(&heap);
+    for(int i = 0; i < heap.elementCount; ++i){
+        int curr;
+        Heap_fetchTop(&heap, &curr);
+        ASSERT(prev >= curr);
+        prev = curr;
+        Heap_removeTop(&heap);
+    }
+    CLEAN_HEAP();   
+}
+
+
 GREATEST_SUITE(Heap_suite) 
 {
     RUN_TEST(Heap_basic_test);
+    RUN_TEST(Heap_random_test);
 }
